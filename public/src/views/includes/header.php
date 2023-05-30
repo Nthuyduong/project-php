743e78faa09f2dae9e1f52f51efc84a5b0d9e07a
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-05-30 16:02:10 +0700

    headnav

--- public/src/views/includes/header.php
+++ public/src/views/includes/header.php
@@ -12,7 +12,7 @@
         <div>
             <?php include 'bag.php'; ?>
         </div>
-            <div class="headnav">
+            <div id="headnav">
                 <!--HEADER-->
                 <header class="section top-page">
                     <div class="header card-header border-0 container">
@@ -45,7 +45,7 @@
                                     <div class="search-box">
                                         <input type="text" placeholder="search...">
                                         <a href="search.php" class="iconsearch">
-                                        <i class="fab fa-search fa-lg"></i>
+                                        <i class="fa fa-search fa-lg"></i>
                                         </a>
                                     </div>
                                     <!--search-button-end-->
@@ -76,6 +76,18 @@
                     </div>
                 </nav>
             </div>
+            <script>
+                var prevScrollpos = window.pageYOffset;
+                window.onscroll = function() {
+                var currentScrollPos = window.pageYOffset;
+                if (prevScrollpos > currentScrollPos) {
+                    document.getElementById("headnav").style.top = "0";
+                } else {
+                    document.getElementById("headnav").style.top = "-150px";
+                }
+                prevScrollpos = currentScrollPos;
+                }
+            </script>
             <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
