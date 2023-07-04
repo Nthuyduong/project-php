3e38242a189c5b64c71f660903857bc0b753c7e2
Hien Tran <109644638+hienttm@users.noreply.github.com>
2023-07-05 02:18:58 +0700

    fix lỗi đóng mở tab

--- src/controllers/controller_user_update.php
+++ src/controllers/controller_user_update.php
@@ -1,31 +1,19 @@
 <?php
 session_start();
 require_once("../models/model_user.php");
-if(isset($_REQUEST["b1"])==false)
-{   
-    $alert_title="Bạn chưa update tài khoàn thành công";
-    $alert = "Quay về trang home";
-    $link_tieptuc="../views/index/home.php";
-    require_once("../views/includes/alert.php");
-    die();
-}
-$email=$_SESSION["user_email"];
-$name=$_REQUEST["username"];
-$phone=$_REQUEST["userphone"];
-$address=$_REQUEST["useraddress"];
-
+$email = $_POST["email"];
+$username = $_POST["username"];
+$userphone = $_POST["userphone"];
+$useraddress = $_POST["useraddress"];
 $user=new model_user();
-$ketqua=$user->UpdateUserAccount($name,$phone,$address,$email);
+$ketqua=$user->UpdateUserAccount($username,$userphone,$useraddress,$email);
 if($ketqua==false){
-    $alert_title="Loi truy van CSDL";
-    $alert = "Loi truy van CSDL";
+    echo "FAIL";
 }else{
-    $alert_title="Update Thành công";
-    $alert = "Quay lại trang user account";
-    $link_tieptuc="../views/index/user-account.php";
-    $_SESSION["user_name"]=$name;
-    $_SESSION["user_phone"]=$phone;
-    $_SESSION["user_address"]=$address;
+    echo "success";
+    
+    $_SESSION["user_name"]=$username;
+    $_SESSION["user_phone"]=$userphone;
+    $_SESSION["user_address"]=$useraddress;
 }
-include_once("../views/includes/alert.php");
 ?>
--- src/views/index/user-account.php
+++ src/views/index/user-account.php
@@ -9,6 +9,8 @@ require_once("../../core/checklogin_user.php");
         <title>User account</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/c813cf59a3.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
         <link rel="stylesheet" href="<?php echo URLROOT?>/public/css/fonts.css">
         <link rel="stylesheet" href="<?php echo URLROOT?>/public/css/user-account.css">
         <link rel="stylesheet" href="<?php echo URLROOT?>/public/css/style.css">
@@ -38,71 +40,57 @@ require_once("../../core/checklogin_user.php");
                 <div class="line mt-40 mt-60"></div>
                 <!-- CONTENT ALL HERE -->
                 <div >
-                    <div class="row">
-                        <!-- SIDE BAR HERE -->
-                        <!-- Create tabs to change content -->
-                            <div class="col-2">
-                                <div class="user-nav nav nav-tabs" id="myTab" role="tablist">
-                                    <ul class="nav nav-tabs" id="myTab" role="tablist">
-                                        <li class="nav-item" role="presentation">
-                                            <a  class="nav-link active" id="info-tab" data-bs-toggle="tab" data-bs-target="#infor" type="button" role="tab" aria-controls="infor" aria-selected="true">
-                                                Personal Information
-                                            </a>
-                                        </li>
-                                        <li class="nav-item" role="presentation">
-                                            <a class="nav-link" id="order-tab" data-bs-toggle="tab" data-bs-target="#order" type="button" role="tab" aria-controls="order" aria-selected="false">
-                                                Order History
-                                            </a>
-                                        </li>
-                                        <li class="nav-item" role="presentation">
-                                            <a class="nav-link" id="pass-tab" data-bs-toggle="tab" data-bs-target="#pass" type="button" role="tab" aria-controls="pass" aria-selected="false">
-                                                Password
-                                            </a>
-                                        </li>
-                                        <li class="nav-item" role="presentation">
-                                            <a class="nav-link" id="notify-tab" data-bs-toggle="tab" data-bs-target="#notify" type="button" role="tab" aria-controls="notify" aria-selected="false">
-                                                Notification
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <!-- CUSTOMER INFORMATION TAB -->
-                            <div class="col-10">
-                                <div class="tab-content" id="myTabContent">
-                                    <div class="tab-pane fade show active" id="infor" role="tabpanel" aria-labelledby="info-tab">
-                                    <form action="../../controllers/controller_user_update.php" method="post">
-                                        <div class="row">
-                                            
-                                                <div class="col-6">
-                                                    <div>
-                                                        <label for="usermail">Email Address</label>
-                                                        <p for="usermail"><?=$_SESSION["user_email"]?></p>
-                                                    </div>
-                                                    <div class="my-3">
-                                                        <label for="username">Full Name</label>
-                                                        <input class="inpu" name="username" id="username" value="<?=$_SESSION["user_name"]?>"/>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6">
-                                                    <div>
-                                                        <label for="userphone">Phone Number</label>
-                                                        <input class="inpu" name="userphone" id="userphone" value="<?=$_SESSION["user_phone"]?>"/>
-                                                    </div>
-                                                    <div class="my-3">
-                                                        <label for="useradd">Address</label>
-                                                        <input class="inpu" name="useraddress" id="useraddress" value="<?=$_SESSION["user_address"]?>"/>
-                                                    </div>
-                                                    <!-- <div class="btn btn-pri">Edit information</div> -->
-                                                    <input type="submit" class="btn btn-pri" name="b1" id="b1" value="Edit"/>
-                                                </div>
-                                            
+                <div class="row">
+                    <div class="col-2">
+                        <div class="user-nav nav nav-tabs" id="myTab" role="tablist">
+                        <a class="nav-link active" id="info-tab" data-bs-toggle="tab" data-bs-target="#infor" type="button" role="tab" aria-controls="infor" aria-selected="true">
+                            Personal Information
+                        </a>
+                        <a class="nav-link" id="order-tab" data-bs-toggle="tab" data-bs-target="#order" type="button" role="tab" aria-controls="order" aria-selected="false">
+                            Order History
+                        </a>
+                        <a class="nav-link" id="pass-tab" data-bs-toggle="tab" data-bs-target="#pass" type="button" role="tab" aria-controls="pass" aria-selected="false">
+                            Password
+                        </a>
+                        <a class="nav-link" id="notify-tab" data-bs-toggle="tab" data-bs-target="#notify" type="button" role="tab" aria-controls="notify" aria-selected="false">
+                            Notification
+                        </a>
+                        </div>
+                    </div>
+                    <div class="col-10">
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="infor" role="tabpanel" aria-labelledby="info-tab">
+                                <form id="updateform" name="inforuser">
+                                <!-- Thêm nội dung của tab "Personal Information" vào đây -->
+                                    <div class="row">
+                                                    
+                                        <div class="col-6">
+                                            <div>
+                                                <label for="usermail">Email Address</label>
+                                                <p id="email" for="usermail"><?=$_SESSION["user_email"]?></p>
+                                            </div>
+                                            <div class="my-3">
+                                                <label for="username">Full Name</label>
+                                                <input class="inpu" name="username" id="username" value="<?=$_SESSION["user_name"]?>"/>
+                                            </div>
                                         </div>
-                                        </form>
+                                        <div class="col-6">
+                                            <div>
+                                                <label for="userphone">Phone Number</label>
+                                                <input class="inpu" name="userphone" id="userphone" value="<?=$_SESSION["user_phone"]?>"/>
+                                            </div>
+                                            <div class="my-3">
+                                                <label for="useradd">Address</label>
+                                                <input class="inpu" name="useraddress" id="useraddress" value="<?=$_SESSION["user_address"]?>"/>
+                                            </div>
+                                            <!-- <div class="btn btn-pri">Edit information</div> -->
+                                            <input type="submit" class="btn btn-pri" name="b1" id="b1" value="Edit"/>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="tab-pane fade" id="order" role="tabpanel" aria-labelledby="order-tab">
-                                    <!-- ORDER HISTORY TAB -->
+                                </form>
+                            </div>
+                            <div class="tab-pane fade" id="order" role="tabpanel" aria-labelledby="order-tab">
+                                <!-- Thêm nội dung của tab "Order History" vào đây -->
                                     <div class="col-10">
                                         <div class="tbl">
                                             <div class="tb-row title-row">
@@ -151,75 +139,105 @@ require_once("../../core/checklogin_user.php");
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="tab-pane fade" id="pass" role="tabpanel" aria-labelledby="pass-tab">
-                                    <!-- CHANGE PASSWORD TAB -->
-                                    <div class="row">
-                                        <div class="col-2"></div>
-                                        <div class="col-8">
-                                            <h4 class="mb-5 alg-center">Change Your Password Here!</h4>
-                                            <div>
-                                                <label for="uservermail">Email Address</label>
-                                                <input class="inpu" name="uservermail" id="uservermail" placeholder=""/>
-                                            </div>
-                                            <div class="my-3">
-                                                <label for="usercurpass">Current Password</label>
-                                                <input class="inpu" name="usercurpass" id="usercurpass" placeholder=""/>
-                                            </div>
-                                            <div>
-                                                <label for="userretype">Re-type Current Password</label>
-                                                <input class="inpu" name="userretype" id="userretype" placeholder=""/>
-                                            </div>
-                                            <div class="row mt-5">
-                                                <div class="col-6 ps-0">
-                                                    <div class="w-100 btn btn-sec">Cancel</div>
-                                                </div>
-                                                <div class="col-6 pe-0">
-                                                    <div class="w-100 btn btn-pri">Change Password</div>
-                                                </div>
-                                            </div>
+                            </div>
+                        </div> 
+                        <div class="tab-pane fade" id="pass" role="tabpanel" aria-labelledby="pass-tab">
+                            <!-- CHANGE PASSWORD TAB -->
+                            <div class="row">
+                                <div class="col-2"></div>
+                                <div class="col-8">
+                                    <h4 class="mb-5 alg-center">Change Your Password Here!</h4>
+                                    <div>
+                                        <label for="uservermail">Email Address</label>
+                                        <input class="inpu" name="uservermail" id="uservermail" placeholder=""/>
+                                    </div>
+                                    <div class="my-3">
+                                        <label for="usercurpass">Current Password</label>
+                                        <input class="inpu" name="usercurpass" id="usercurpass" placeholder=""/>
+                                    </div>
+                                    <div>
+                                        <label for="userretype">Re-type Current Password</label>
+                                        <input class="inpu" name="userretype" id="userretype" placeholder=""/>
+                                    </div>
+                                    <div class="row mt-5">
+                                        <div class="col-6 ps-0">
+                                            <div class="w-100 btn btn-sec">Cancel</div>
                                         </div>
-                                        <div class="col-2"></div>
+                                        <div class="col-6 pe-0">
+                                            <div class="w-100 btn btn-pri">Change Password</div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="tab-pane fade" id="notify" role="tabpanel" aria-labelledby="notify-tab">
-                                    <!-- NOTIFICATION TAB???? -->
-                                    <div>Notifications show here</div>
-                                </div>
+                                <div class="col-2"></div>
                             </div>
+                        </div>
+                        <div class="tab-pane fade" id="notify" role="tabpanel" aria-labelledby="notify-tab">
+                            <!-- NOTIFICATION TAB???? -->
+                            <div>Notifications show here</div>
+                        </div>   
+                    </div>
                 </div>
             </div>
         </div>
+    
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
         <?php require_once '../includes/footer.php';?>
         <script>
-        $(document).ready(function() {
-            $('#b1').click(function() {
-                // Lấy giá trị từ các trường input
-                var username = $('#username').val();
-                var userphone = $('#userphone').val();
-                var address = $('#useraddress').val();
-
-                // Gửi yêu cầu Ajax
+            function updateAJAX(event)
+            {   
+                event.preventDefault();
+                email = $("#email").text();
+                username = $("#username").val(); 
+                userphone = $("#userphone").val();
+                useraddress = $("#useraddress").val();
+            
                 $.ajax({
-                url: 'update.php',  // Đường dẫn tới file xử lý cập nhật trên server
-                method: 'POST',
-                data: {
-                    column1: column1Value,
-                    column2: column2Value
-                },
-                success: function(response) {
-                    // Xử lý kết quả trả về nếu cần
-                    console.log(response);
-                },
-                error: function(xhr, status, error) {
-                    // Xử lý lỗi nếu có
-                    console.error(error);
-                }
+                    url: "../../controllers/controller_user_update.php",
+                    type: "POST",
+                    data: {
+                        email:email,
+                        username: username,
+                        userphone:userphone,
+                        useraddress:useraddress},
+                        success: function(responseData,status) {
+                        if(status=="success"){
+                            alert("Thành công");
+                        }
+                        else
+                            alert("ERROR");
+                    }
+                });
+            } 
+            $(document).ready(function () {
+                $("#updateform").submit(updateAJAX);
+            });  
+        </script>
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                // Lắng nghe sự kiện khi click vào các tab
+                const tabLinks = document.querySelectorAll(".nav-link");
+                tabLinks.forEach(function(link) {
+                    link.addEventListener("click", function(e) {
+                        e.preventDefault();
+                        // Xóa lớp active của tất cả các tab
+                        tabLinks.forEach(function(tab) {
+                            tab.classList.remove("active");
+                        });
+                        // Thêm lớp active cho tab được click
+                        link.classList.add("active");
+                        // Ẩn tất cả nội dung của các tab
+                        const tabContents = document.querySelectorAll(".tab-pane");
+                        tabContents.forEach(function(content) {
+                            content.classList.remove("show", "active");
+                        });
+                        // Hiển thị nội dung của tab tương ứng với tab được click
+                        const target = link.getAttribute("data-bs-target");
+                        const tabContent = document.querySelector(target);
+                        tabContent.classList.add("show", "active");
+                    });
                 });
             });
-        });
         </script>
     </body>
 </html>
