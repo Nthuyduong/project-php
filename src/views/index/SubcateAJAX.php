06e5ab9156a6007ae50c05e03e0ffe0a40e5e821
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-07-08 20:36:09 +0700

    commit

--- src/views/index/SubcateAJAX.php
+++ src/views/index/SubcateAJAX.php
@@ -1,5 +1,3 @@
-<?php define('URLROOT', 'http://localhost:8888/project-php'); ?>
-<link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/admin-style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script src="https://kit.fontawesome.com/c813cf59a3.js" crossorigin="anonymous"></script>
 <?php
